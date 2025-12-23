df3d9184fabed35f7dd5c5bee857babf250c9c89
Its-Clement <ping.clementbabu@gmail.com>
2025-12-23 03:21:18 +0000

    Time: 1 ms (55.56%) | Memory: 57.1 MB (81.82%) - LeetSync

---
+++ 2058-concatenation-of-array/concatenation-of-array.js
@@ -0,0 +1,13 @@
+/**
+ * @param {number[]} nums
+ * @return {number[]}
+ */
+var getConcatenation = function(nums) {
+    const n = nums.length;
+    var result = new Array(n * 2);
+    for (let i = 0; i < n; i++){
+        result[i] = nums[i];
+        result[i + n] = nums[i];
+    }
+    return result;
+};
